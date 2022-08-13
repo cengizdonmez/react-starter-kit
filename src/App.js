0c525b81b13fabc004b72f1d5f5412dedbbd7c5a
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-08-14 02:46:13 +0300

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import './tailwind.css'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-blue-500 hover:bg-green-400 transition'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
