97ad0cb7dbe31d3eaacd99f27148fbbb1af40895
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-08-16 02:07:28 +0300

    useState Hook example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './tailwind.css'
-
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('Cengiz');
 
+  const handleClick = () => {
+    setName('Beyza')
+  }
 
   return (
-    <div className="App bg-slate-800 h-screen text-white">
-      
+    <div className="app">
+      <h2>useState</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Change Name</button>
+      {/* <button onClick={() => setName('Beyza')}>Change Name</button> */}
     </div>
   );
 }
