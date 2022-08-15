9880275ee7262d219d748dd041575477d8e71ea1
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-08-16 01:05:37 +0300

    past example cleared

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import logo from './logo.svg';
 import './tailwind.css'
 
-import BaseButton from './components/BaseButton';
 
 function App() {
 
-  const todos = ['Learn React', 'Learn Tailwind', 'Build a todo app']
 
   return (
-    <div className="App bg-slate-800 h-screen">
-      <header className="App-header">
-        <img src={logo} className="App-logo w-72 mx-auto" alt="logo" />
-      </header>
-      <main className="App-main">
-        <BaseButton> Hello World </BaseButton>
-        <BaseButton variant='success'> Hello World </BaseButton>
-        <BaseButton variant='danger'> Hello World </BaseButton>
-        <BaseButton variant='warning'> Hello World </BaseButton>
-        <BaseButton variant='info'> Hello World </BaseButton>
-   
-        <div className="md:container mx-auto text-white">
-            <ul>
-              {todos.map(todo => (
-                <li key={todo}>{todo}</li>
-              ))}
-            </ul>
-        </div>
-      </main>
+    <div className="App bg-slate-800 h-screen text-white">
+      
     </div>
   );
 }
