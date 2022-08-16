94df820bfbb30f0d073752bd728697f4165d7f6b
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-08-17 02:41:19 +0300

    props example

--- src/example/OutputtingListsExp.js
+++ src/example/OutputtingListsExp.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "../components/BlogList.js";
 
 const OutputtingListsExp = () => {
   const [blogs, setBlogs] = useState([
@@ -9,15 +10,7 @@ const OutputtingListsExp = () => {
 
   return (
     <div>
-       {blogs.map((blog) => (
-        <div className="flex-1 truncate py-3 transition-all hover:bg-slate-200" key={blog.id}>
-          <div className="flex items-center space-x-3">
-            <h3 className="text-gray-900 text-sm font-medium truncate">{blog.title}</h3>
-            <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{blog.author}</span>
-          </div>
-          <p className="mt-1 text-gray-500 text-sm truncate">{blog.subtitle}</p>
-        </div>
-       ))}
+       <BlogList blogs={blogs} title="All Blogs"/>
     </div>
   )
 }
