a1bbebd6d1e77bf73735f968aceaf44c37717575
Cengiz Dönmez <c.donmezzz@gmail.com>
2022-08-18 04:13:44 +0300

    useEffect example works when counter changes

--- src/example/UseEffectExp.js
+++ src/example/UseEffectExp.js
@@ -6,7 +6,7 @@ const UseEffectExp = () => {
   useEffect(() => {
     //Component ilk yüklendiğinde çalışır.
     console.log("useEffect");
-  }, []); // [] => component tekrar çalışmaz.
+  }, [counter]); //counter değiştiğinde çalışır.
 
   const increase = () => {
     setCounter(counter + 1);
